8a347238d8c5b3bd43a69728b0dd43cdd1458d04
toqitahmid <rubaiathali@gmail.com>
2024-11-09 09:35:27 +0600

    Add new massege

---
+++ Hello.c
@@ -0,0 +1,17 @@
+
+// Write a program that prints some message!
+
+#include<stdio.h>
+int main()
+
+{
+
+    char str[10];
+    printf("Enter your name : ");
+    scanf("%s", &str);
+
+    printf("Hello, %s", str);
+
+    return 0;
+
+}
