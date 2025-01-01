f0f53b34af1564b6e4cea8fea71d145e35929fd2
Toqi Tahmid <rubaiathali@gmail.com>
2025-01-01 19:43:24 +0600

    print a massage

---
+++ Input-Output/hello.c
@@ -0,0 +1,17 @@
+
+// Wirte a programm that prints a massage
+
+#include<stdio.h>
+int main()
+
+{
+    char name[20];
+
+    printf("Enter your name : ");
+    scanf("%s", name);
+
+    printf("Assalamualaikum %s", name);
+
+    return 0;
+    
+}
