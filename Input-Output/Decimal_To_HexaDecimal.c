c6825fe6afc50622b01c699b065ca7aa5a8a798d
Toqi Tahmid <rubaiathali@gmail.com>
2025-01-03 19:05:08 +0600

    Convert decimal into hexa-decimal

---
+++ Input-Output/Decimal_To_HexaDecimal.c
@@ -0,0 +1,18 @@
+
+// Wirte a program that convert Decimal number into Hexa-Decimal
+
+#include<stdio.h>
+int main()
+
+{
+
+    int number;
+
+    printf("Enter any decimal number : ");
+    scanf("%d", &number);
+
+    printf("The Hexa-decimal number of the decimal number is : %x", number);
+
+    return 0;
+
+}
