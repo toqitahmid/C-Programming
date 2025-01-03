66645c7292fa9f946da861ef98fbbbfb2b4e76cc
Toqi Tahmid <rubaiathali@gmail.com>
2025-01-03 19:07:29 +0600

    Convert decimal into octal

---
+++ Input-Output/Decimal_To_Octal.c
@@ -0,0 +1,17 @@
+
+// Write a program that convert decimal into octal number.
+
+#include<stdio.h>
+int main()
+
+{
+    int number;
+
+    printf("Enter any decimal number : ");
+    scanf("%d", &number);
+
+    printf("The octal number of the decinmal is : %o", number);
+    
+    return 0;
+
+}
