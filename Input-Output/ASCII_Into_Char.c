9bb2af9fca76524425c4a79cb29607dd2ad27818
Toqi Tahmid <rubaiathali@gmail.com>
2025-01-03 16:08:58 +0600

    Convert ASCII value into character

---
+++ Input-Output/ASCII_Into_Char.c
@@ -0,0 +1,17 @@
+
+// Write a programm that convert ASCII value into Character.
+
+#include<stdio.h>
+int main()
+
+{
+    int apple;
+
+    printf("Enter any ASCII value : ");
+    scanf("%d", &apple);
+
+    printf("OK, The character of the ASCII value is : %c", apple);
+
+    return 0;
+    
+}
