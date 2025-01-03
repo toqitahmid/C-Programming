8403ec83a8eb220f2d8259040dd5e6538c4b3903
Toqi Tahmid <rubaiathali@gmail.com>
2025-01-03 16:23:01 +0600

    Covert character into ASCII value

---
+++ Input-Output/Char_Into_ASCII.c
@@ -0,0 +1,17 @@
+
+// Write a programm that convert character into ASCII value
+
+#include<stdio.h>
+int main()
+
+{
+    char Egg;
+
+    printf("Enter any character : ");
+    scanf("%c", &Egg);
+
+    printf("OK, The ASCII value of the character is : %d", Egg);
+
+    return 0;
+
+}
