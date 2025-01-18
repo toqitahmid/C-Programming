c8309192b1234a61104c988cfcde416c0328faa6
Toqi Tahmid <rubaiathali@gmail.com>
2025-01-19 00:12:15 +0600

    sum with while loop

---
+++ Loop/switch.c
@@ -0,0 +1,53 @@
+
+// Write a progarm using switch case
+
+#include<stdio.h>
+int main()
+
+{
+    int number;
+
+    printf("Enter any integer number : ");
+    scanf("%d", &number);
+
+    switch (number) {
+
+        case 0 : 
+
+        printf("Zero");
+        break;
+
+        case 1 :
+
+        printf("One");
+        break;
+
+        case 2 : 
+
+        printf("Tow");
+        break;
+
+        case 3 : 
+
+        printf("Three");
+        break;
+
+        case 4 : 
+
+        printf("Four");
+        break;
+
+        case 5 : 
+
+        printf("Five");
+        break;
+
+        default : 
+
+        printf("This is not a valid digit!");
+
+
+    }
+
+    return 0;
+}
